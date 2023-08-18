39059c4cf627ba794bb933e011774685ec188d5f
tagtart1 <adamtbizz@gmail.com>
2023-08-18 15:41:52 -0500

    Change Login api to send back an httpOnly cookie with user data

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,22 +3,34 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 exports.postLogin = asyncHandler(async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  // Authenticate later
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    // Authenticate later
 
-  const userInfo = {
-    username: user.username,
-    id: user._id,
-  };
+    const userInfo = {
+      username: user.username,
+      id: user._id,
+    };
 
-  jwt.sign(
-    { user: userInfo },
-    "secretkey",
-    { expiresIn: "3h" },
-    (err, token) => {
-      res.json({
-        token: token,
-      });
-    }
-  );
+    jwt.sign(
+      { user: userInfo },
+      "secretkey",
+      { expiresIn: "3h" },
+      (err, token) => {
+        res.cookie("token", token, {
+          httpOnly: true,
+          maxAge: 10800000,
+        });
+
+        res.json({
+          success: true,
+          data: userInfo,
+        });
+      }
+    );
+  } catch (err) {
+    res.status(401).json({
+      message: "Invalid credentials",
+    });
+  }
 });
