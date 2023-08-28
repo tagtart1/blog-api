4c8424ab02c70358880591dd0fc7ad08936ef092
tagtart1 <adamtbizz@gmail.com>
2023-08-28 14:16:33 -0500

    Change verifytoken to just add token to the req token field

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,19 +1,15 @@
 // Verify token
 const verifyToken = (req, res, next) => {
-  // Get auth header value
-  const bearerHeader = req.headers["authorization"];
+  const token = req.cookies.token;
 
-  if (typeof bearerHeader !== "undefined") {
-    const bearer = bearerHeader.split(" ");
-    // Get token
-    const bearerToken = bearer[1];
-    // Set the token
-    req.token = bearerToken;
-
+  if (token) {
+    req.token = token;
     next();
   } else {
     // Can send custom message later
-    res.sendStatus(403);
+    res.status(403).json({
+      message: "Failed to authorize token",
+    });
   }
 };
 
