d73f0fc25805238205a9aae66cc1066b8c209971
tagtart1 <adamtbizz@gmail.com>
2023-09-05 19:13:32 -0500

    Temporary script to rename docuement fields

---
+++ rename.js
@@ -0,0 +1,21 @@
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+const Post = require("./models/post");
+
+const run = async () => {
+  // Set up mongoose connection
+  const mongoose = require("mongoose");
+  mongoose.set("strictQuery", false);
+
+  const main = async () => {
+    await mongoose.connect(process.env.MONGODB);
+  };
+  main().catch((err) => {
+    console.log(err);
+  });
+
+  await Post.updateMany({}, { $rename: { timestamp: "createdTimestamp" } });
+};
+
+run();
